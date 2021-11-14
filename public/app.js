4b4cd129736baa5e6a00dfae051bd3a52805df80
Idris Bankole <bankoleidris@gmail.com>
2021-11-14 10:16:03 +0100

    interfaces and formatters

--- public/app.js
+++ public/app.js
@@ -1,5 +1,13 @@
 //  interfaces with class
 import { Invoice } from "./classes/invoice.js";
+let docOne;
+let docTwo;
+docOne = new Invoice("yoshi", "web work", 250);
+docTwo = new Invoice("mario", "plumbing work", 200);
+let docs = [];
+docs.push(docOne);
+docs.push(docTwo);
+console.log(docs);
 const invOne = new Invoice("mario", "work on the mario website", 250);
 const invTwo = new Invoice("luigi", "work on the mario website", 350);
 let invoices = [];
--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,18 @@ import { Invoice } from "./classes/invoice.js";
 import { Payment } from "./classes/payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
 
+let docOne: HasFormatter;
+let docTwo: HasFormatter;
+
+docOne = new Invoice("yoshi", "web work", 250);
+docTwo = new Invoice("mario", "plumbing work", 200);
+
+let docs: HasFormatter[] = [];
+docs.push(docOne);
+docs.push(docTwo);
+
+console.log(docs);
+
 const invOne = new Invoice("mario", "work on the mario website", 250);
 
 const invTwo = new Invoice("luigi", "work on the mario website", 350);
