c8068ffb544903d0fa72b2b83470d14821e67264
Idris Bankole <bankoleidris@gmail.com>
2021-11-14 10:38:03 +0100

    rendering HTML template

--- public/classes/ListTemplate.js
+++ public/classes/ListTemplate.js
@@ -1,1 +1,11 @@
-"use strict";
+export class ListTemplate {
+    constructor(container) {
+        this.container = container;
+    }
+    render(item, heading, pos) {
+        const li = document.createElement("li");
+        const h4 = document.createElement("h4");
+        h4.innerText = heading;
+        li.append(h4);
+    }
+}
---
+++ src/classes/ListTemplate.ts
@@ -0,0 +1,12 @@
+import { HasFormatter } from "../interfaces/HasFormatter.js";
+
+export class ListTemplate {
+  constructor(private container: HTMLUListElement) {}
+  render(item: HasFormatter, heading: string, pos: "start" | "end") {
+    const li = document.createElement("li");
+    const h4 = document.createElement("h4");
+
+    h4.innerText = heading;
+    li.append(h4);
+  }
+}
